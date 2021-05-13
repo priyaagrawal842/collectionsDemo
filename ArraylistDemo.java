14002e820b2aa90a2931a89bbdc037d1328f0bb8
Priya Agrawal <20261087+priyaagrawal842@users.noreply.github.com>
2021-05-13 22:08:43 +0530

    added object insertion to a ArrayList

--- ArraylistDemo.java
+++ ArraylistDemo.java
@@ -5,7 +5,7 @@ public class ArraylistDemo {
 
     public static void main(String[] args)
     {
-
+        //Inserting integer values
         List<Integer> list1 = new ArrayList<>();
         list1.add(101);
         list1.add(102);
@@ -13,6 +13,17 @@ public class ArraylistDemo {
         list1.add(104);
 
         System.out.println(list1);
+
+        //Inserting an Employee object
+            List<Employee> employeeList= new ArrayList<>();
+            employeeList.add(new Employee ("1", "priya","agrawal"));
+            for(Employee e: employeeList)
+                {
+                    System.out.println(e.getId()   +  e.getName() + e.getSurname());
+                }
+
+
+
 
 
     }
