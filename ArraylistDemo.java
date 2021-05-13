74f59748fb94ed7898f0affe48425b20aeb9957f
Priya Agrawal <20261087+priyaagrawal842@users.noreply.github.com>
2021-05-13 21:58:42 +0530

    arraylist insertion demo

---
+++ ArraylistDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+
+    public static void main(String[] args)
+    {
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(101);
+        list1.add(102);
+        list1.add(103);
+        list1.add(104);
+
+        System.out.println(list1);
+
+
+    }
+}
